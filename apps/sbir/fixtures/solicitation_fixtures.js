8e1f1505a079c1634f09cb715faa75197280ddd8
Luc Castera <luc.castera@gmail.com>
2011-11-06 08:39:19 -0500

    created solicitation model

---
+++ apps/sbir/fixtures/solicitation_fixtures.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   Sbir.Solicitation Fixtures
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Sbir */
+
+sc_require('models/solicitation_model');
+
+Sbir.Solicitation.FIXTURES = [
+
+  {
+    guid: 1,
+    title: "A new method for creating Solar Panels using hydrogen peroxide"
+  },
+  {
+    guid: 2,
+    title: "Farming technology to produce mass amount of grass-fed beef"
+  },
+  {
+    guid: 3,
+    title: "A new paradigm for solicitating feedback from peers for government agencies"
+  }
+
+];
---
+++ apps/sbir/models/solicitation_model.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Sbir.Solicitation
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Sbir */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Sbir.Solicitation = SC.Record.extend(
+/** @scope Sbir.Solicitation.prototype */ {
+
+  title: SC.Record.attr(String)
+
+}) ;
