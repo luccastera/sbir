33eba36a72a74ff4ccf8a396a067d84f5ee1b05b
Luc Castera <luc.castera@gmail.com>
2011-11-17 18:47:13 -0500

    Added localStorage via Cookie for browsers who do not support it

--- apps/sbir/main.js
+++ apps/sbir/main.js
@@ -4,6 +4,47 @@
 // ==========================================================================
 /*globals Sbir */
 
+function createCookie(name, value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 50 * 60 * 1000 ));
+    var expires = "; expires=" + date.toGMTString();
+  } else {
+    var expires = "";
+  }
+  document.cookie = name  + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+  var result = "";
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) {
+      result = c.substring(nameEQ.length, c.length);
+    } else {
+      result = "";
+    }
+  }
+  return result;
+}
+
+if (!window.localStorage) {
+  console.log('define localStorage')
+  localStorage = (function() {
+    return {
+      setItem: function(key, value) {
+        createCookie(key,value, 3000);
+      },
+      getItem: function(key) {
+        return readCookie(key);
+      }
+    }
+  })
+}
+
 // This is the function that will start your app running.  The default
 // implementation will load any fixtures you have created then instantiate
 // your controllers and awake the elements on your page.
